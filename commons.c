a6049fa26f3e15e10092c76641709c83a82652b1
yehouda Lejdstrom <yehouda.lejdstrom@gmail.com>
2023-05-08 15:25:44 +0300

    Add prototype of commons functions

---
+++ commons.c
@@ -0,0 +1,14 @@
+#include "commons.h"
+
+int send_file_to_socket(const char * filename, int socket, DIRECTION direction)
+{
+
+}
+int recv_file_from_socket(const char * filename, int socket, DIRECTION direction)
+{
+
+}
+int get_file_size(FILE * file)
+{
+
+}
--- commons.h
+++ commons.h
@@ -1,3 +1,34 @@
 #ifndef COMMONS_H
 #define COMMONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <dirent.h>
+
+
+#define BUFF_SIZE (1024)
+#define LOCAL_HOST "127.0.0.1"
+
+typedef enum{
+    LIST, UPLOAD, DOWNLOAD, EXIT /*MKDIR, CD*/
+}FTP_COMMANDS;
+
+typedef enum{
+    SERVER_TO_CLIENT, CLIENT_TO_SERVER
+}DIRECTION;
+
+FTP_COMMANDS parse_ftp_cmd(const char * client_message);
+
+int send_file_to_socket(const char * filename, int socket, DIRECTION direction);
+int recv_file_from_socket(const char * filename, int socket, DIRECTION direction);
+int get_file_size(FILE * file);
+
+
 #endif
